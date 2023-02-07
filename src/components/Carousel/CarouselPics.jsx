850f7b2d7c37d1aad6314662b95b7e0dc60667c6
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-02-08 00:18:01 +0500

    chore: carousel responsiveness

--- src/components/Carousel/CarouselPics.jsx
+++ src/components/Carousel/CarouselPics.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 
 export function CarouselPics({ children }) {
-	const [activeIndex, setActiveIndex] = React.useState(0);
+	const [activeIndex, setActiveIndex] = useState(0);
+	const carouselContainerRef = useRef(null);
 
 	function updateIndex(newIndex) {
 		if (newIndex < 0) {
@@ -12,23 +13,31 @@ export function CarouselPics({ children }) {
 		}
 		setActiveIndex(newIndex);
 	}
-
 	return (
-		<section className='flex justify-between'>
-			<button onClick={() => updateIndex(activeIndex - 1)}>
+		<section className='flex justify-between items-center'>
+			<button
+				onClick={() => updateIndex(activeIndex - 1)}
+				className='h-fit md:block hidden'
+			>
 				<ArrowLeftIcon className='w-12 h-12 transition-all text-colorThree hover:text-colorOne' />
 			</button>
-			<div className='overflow-hidden mx-20 w-full'>
+			<div
+				className='overflow-hidden xl:mx-20 lg:mx-14 md:mx-6 sm:mx-2 w-full'
+				ref={carouselContainerRef}
+			>
 				<div
 					className='whitespace-nowrap transition-transform duration-300'
-					style={{ transform: `translateX(-${activeIndex * 250}px)` }}
+					style={{ transform: `translateX(-${activeIndex * 312}px)` }}
 				>
 					{React.Children.map(children, (child, index) => {
 						return React.cloneElement(child, { width: '300px' });
 					})}
 				</div>
 			</div>
-			<button onClick={() => updateIndex(activeIndex + 1)}>
+			<button
+				onClick={() => updateIndex(activeIndex + 1)}
+				className='h-fit md:block hidden'
+			>
 				<ArrowRightIcon className='w-12 h-12 transition-all text-colorThree hover:text-colorOne' />
 			</button>
 		</section>
