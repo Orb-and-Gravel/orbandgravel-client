3bd45c85e1f76871c8ff49937e8b1d4c91a0b4c8
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-03-19 03:33:42 +0500

    feat: radial made

---
+++ src/components/VerticalAlternateList/Radial.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Radial() {
+	return (
+		<div className='bg-red-400 h-44 w-28 rounded-r-full border-r-black border-[28px] border-l-white border-t-white border-b-white '></div>
+	);
+}
--- src/components/VerticalAlternateList/RadialControllerWrapper.jsx
+++ src/components/VerticalAlternateList/RadialControllerWrapper.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Radial } from './Radial';
 
 export function RadialControllerWrapper() {
 	return (
 		<div>
+			<Radial />
 			<h3 className='text-[6rem] font-black font-oswald text-colorFive'>
 				NEW IN
 			</h3>
