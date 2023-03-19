e80dcb732b351b8240d66edfc7f9b9bcfe57346f
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-03-20 00:48:35 +0500

    dev: radial development

--- src/components/VerticalAlternateList/Radial.jsx
+++ src/components/VerticalAlternateList/Radial.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export function Radial() {
 	return (
-		<div className='bg-red-400 h-44 w-28 rounded-r-full border-r-black border-[28px] border-l-white border-t-white border-b-white '></div>
+		<>
+			<div className='bg-green-400 h-64 w-32 rounded-r-full flex items-center'>
+				<div className='bg-white h-56 w-28 rounded-r-full flex items-center'>
+					<div className='bg-red-400 h-40 w-20 rounded-r-full'></div>
+				</div>
+			</div>
+		</>
 	);
 }
