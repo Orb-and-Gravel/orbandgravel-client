33e86bfabad5908772edc36493e919824a02d02c
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-03-24 00:57:01 +0500

    working on radial

--- src/components/VerticalAlternateList/Radial.jsx
+++ src/components/VerticalAlternateList/Radial.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Radial() {
 	return (
 		<div className='relative'>
-			<div className='absolute top-[28%] left-[49%]'>
+			<div className='absolute top-[28%] left-[49%] z-20'>
 				<p className='opacity-50 cursor-pointer hover:scale-110 hover:opacity-100 transition-all'>
 					category 1
 				</p>
@@ -20,9 +20,10 @@ export function Radial() {
 					category 5
 				</p>
 			</div>
-			<div className='box'></div>
+			<div></div>
 			<div className='bg-blue-400 h-[28rem] w-60 rounded-r-full flex items-center'>
-				<div className='bg-white h-[27.5rem] w-[14.7rem] rounded-r-full flex items-center'>
+				<div className='bg-zinc-400 w-36 h-10 absolute top-0'></div>
+				<div className='bg-white h-[27.5rem] w-[14.7rem] rounded-r-full flex items-center z-10'>
 					<div className='bg-green-400 h-64 w-32 rounded-r-full flex items-center'>
 						<div className='bg-white h-56 w-28 rounded-r-full flex items-center'>
 							<div className='bg-red-400 h-40 w-20 rounded-r-full'></div>
