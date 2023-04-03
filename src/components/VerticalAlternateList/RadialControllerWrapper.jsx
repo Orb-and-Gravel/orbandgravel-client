3d69e4175ec548ccea68f88646460e2fa89e16fa
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-04-04 00:23:01 +0500

    feat: responsiveness on 4k screen

--- src/components/VerticalAlternateList/RadialControllerWrapper.jsx
+++ src/components/VerticalAlternateList/RadialControllerWrapper.jsx
@@ -3,10 +3,10 @@ import { Radial } from './Radial';
 
 export function RadialControllerWrapper() {
 	return (
-		<div className='flex '>
+		<div className='flex mt-10'>
 			<Radial />
-			<h3 className='text-[6rem] font-black font-oswald text-colorFive'>
-				NEW IN
+			<h3 className='text-[6rem] font-black font-oswald text-colorFive xl:ml-20 whitespace-nowrap'>
+				NEW IN.
 			</h3>
 		</div>
 	);
