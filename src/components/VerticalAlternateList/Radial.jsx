2dfab86b2d3462982ef8ea4314b935ce5b2c5885
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-04-05 00:16:47 +0500

    feat: radial responsiveness for larger screen handled

--- src/components/VerticalAlternateList/Radial.jsx
+++ src/components/VerticalAlternateList/Radial.jsx
@@ -9,13 +9,13 @@ export function Radial() {
 
 	return (
 		<div className='relative'>
-			<div className='absolute top-[28%] left-[49%] z-20'>
+			<div className='absolute top-[28%] xl:left-[49%] left-[47%] z-20'>
 				<p
 					className={`${
 						catIndex === 0
 							? 'scale-110 font-bold'
 							: 'opacity-50 hover:scale-110 hover:opacity-100 transition-all '
-					} cursor-pointer`}
+					} cursor-pointer text-colorFive xl:text-base text-sm`}
 					onClick={() => handleCatClick(0)}
 				>
 					category 1
@@ -25,7 +25,7 @@ export function Radial() {
 						catIndex === 1
 							? 'scale-110 font-bold'
 							: 'opacity-50 hover:scale-110 hover:opacity-100 transition-all '
-					} cursor-pointer ml-5 mt-5`}
+					} cursor-pointer ml-5 mt-5 text-colorFive xl:text-base text-sm`}
 					onClick={() => handleCatClick(1)}
 				>
 					category 2
@@ -35,7 +35,7 @@ export function Radial() {
 						catIndex === 2
 							? 'scale-110 font-bold'
 							: 'opacity-50 hover:scale-110 hover:opacity-100 transition-all '
-					} cursor-pointer ml-7 mt-5`}
+					} cursor-pointer ml-7 mt-5 text-colorFive xl:text-base text-sm`}
 					onClick={() => handleCatClick(2)}
 				>
 					category 3
@@ -45,7 +45,7 @@ export function Radial() {
 						catIndex === 3
 							? 'scale-110 font-bold'
 							: 'opacity-50 hover:scale-110 hover:opacity-100 transition-all '
-					} cursor-pointer ml-5 mt-5`}
+					} cursor-pointer ml-5 mt-5 text-colorFive xl:text-base text-sm`}
 					onClick={() => handleCatClick(3)}
 				>
 					category 4
@@ -55,24 +55,24 @@ export function Radial() {
 						catIndex === 4
 							? 'scale-110 font-bold'
 							: 'opacity-50 hover:scale-110 hover:opacity-100 transition-all '
-					} cursor-pointer mt-5`}
+					} cursor-pointer mt-5 text-colorFive xl:text-base text-sm`}
 					onClick={() => handleCatClick(4)}
 				>
 					category 5
 				</p>
 			</div>
-			<div className='bg-blue-400 h-[28rem] w-60 rounded-r-full flex items-center overflow-hidden relative'>
+			<div className='bg-colorTwo xl:h-[28rem] xl:w-60 h-[25.5rem] w-[13.3rem] rounded-r-full flex items-center overflow-hidden relative'>
 				<div
-					className='bg-blue-900 absolute top-1/2 w-full h-1/3 transition-all'
+					className='bg-colorFive absolute top-1/2 w-full h-1/3 transition-all'
 					style={{
 						transform: `rotate(${-60 + catIndex * 23}deg) skew(60deg)`,
 						transformOrigin: '0 0',
 					}}
 				></div>
-				<div className='bg-white h-[27.5rem] w-[14.7rem] rounded-r-full flex items-center z-10'>
-					<div className='bg-green-400 h-64 w-32 rounded-r-full flex items-center'>
-						<div className='bg-white h-56 w-28 rounded-r-full flex items-center'>
-							<div className='bg-red-400 h-40 w-20 rounded-r-full'></div>
+				<div className='bg-white xl:h-[27.5rem] xl:w-[14.7rem] h-[25rem] w-[13rem] rounded-r-full flex items-center z-10'>
+					<div className='bg-colorOne xl:h-64 xl:w-32 h-56 w-28 rounded-r-full flex items-center'>
+						<div className='bg-white xl:h-56 xl:w-28 h-48 w-24 rounded-r-full flex items-center'>
+							<div className='bg-colorFour xl:h-40 xl:w-20 h-32 w-16 rounded-r-full'></div>
 						</div>
 					</div>
 				</div>
--- src/components/VerticalAlternateList/RadialControllerWrapper.jsx
+++ src/components/VerticalAlternateList/RadialControllerWrapper.jsx
@@ -3,9 +3,11 @@ import { Radial } from './Radial';
 
 export function RadialControllerWrapper() {
 	return (
-		<div className='flex mt-10'>
-			<Radial />
-			<h3 className='text-[6rem] font-black font-oswald text-colorFive xl:ml-20 whitespace-nowrap'>
+		<div className='flex mt-10 justify-between'>
+			<section className='md:block hidden'>
+				<Radial />
+			</section>
+			<h3 className='xl:text-[6rem] min-[860px]:text-[4rem] md:text-4xl font-black font-oswald text-colorFive min-[860px]:mt-20 2xl:mr-24 xl:mr-20 md:mr-10 whitespace-nowrap'>
 				NEW IN.
 			</h3>
 		</div>
