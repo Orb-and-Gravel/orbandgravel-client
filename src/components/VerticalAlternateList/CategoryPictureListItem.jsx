b367696b51df354a38a7621a400def44b2195c96
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-04-15 03:18:17 +0500

    feat: fixed unwanted height problem

--- src/components/VerticalAlternateList/CategoryPictureListItem.jsx
+++ src/components/VerticalAlternateList/CategoryPictureListItem.jsx
@@ -6,14 +6,14 @@ export function CategoryPictureListItem({
 	imageSrc,
 }) {
 	return (
-		<div className='grid grid-cols-2'>
-			<div className='flex justify-between items-end p-4'>
+		<div className='grid grid-cols-2 h-[250px] w-[700px]'>
+			<div className='flex justify-between items-end p-4 h-[250px]'>
 				<p>{categoryName}</p>
 				<h3 className='text-7xl font-roboto font-bold opacity-40'>
 					{categoryNumber}
 				</h3>
 			</div>
-			<img src={imageSrc} alt='' className='object-cover' />
+			<img src={imageSrc} alt='' className='object-cover h-[250px]' />
 		</div>
 	);
 }
