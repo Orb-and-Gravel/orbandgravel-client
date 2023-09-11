45aa8c6a1e0b8b13c93d560fd348f9bef5aa542d
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-09-11 22:24:03 +0400

    feat: completed featured

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -3,14 +3,14 @@ import { ProductItem } from '../Products/ProductItem';
 
 export function Featured() {
 	return (
-		<div className='grid grid-cols-2 gap-x-12'>
-			<div>
+		<div className='md:grid md:grid-cols-2 md:gap-x-12'>
+			<div className='md:block hidden'>
 				<img
-					className='rounded-xl'
+					className='rounded-xl h-full object-cover'
 					src='https://images.unsplash.com/photo-1601821765780-754fa98637c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
 				/>
 			</div>
-			<div className='grid grid-cols-2 items-center justify-items-center px-20 py-32'>
+			<div className='grid grid-cols-2 items-center justify-items-center 2xl:px-20 2xl:py-32 gap-5'>
 				<ProductItem
 					imgUrl='https://images.unsplash.com/photo-1601821765780-754fa98637c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
 					name='Necklace Name Long'
