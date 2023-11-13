ba0d743020eb537c8c366642b43df803d48e57ce
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-11-13 21:06:54 +0400

    feat: select for sort

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -13,10 +13,27 @@ export function Category() {
 				alt={catData.categoryName}
 				className='w-full h-[25rem] object-cover rounded-xl'
 			/>
-			<section className='mt-4'>
+			<section className='mt-4 flex justify-between'>
 				<h3 className='md:text-4xl lg:text-6xl md:font-oswald text-colorFive whitespace-nowrap text-5xl font-black font-poiret tracking-tight md:block flex justify-center uppercase'>
 					{catData.categoryName}
 				</h3>
+				<div className='flex items-center gap-x-2 w-64'>
+					<p className='whitespace-nowrap'>Sort by</p>
+					{/* !Todo: Replace select and options tag */}
+					<select
+						name='sort'
+						id='sort-categories'
+						className='bg-colorHeader text-colorFour p-2 w-full rounded-md border border-colorTwo'
+					>
+						<option value='popularity' className='bg-white'>
+							Popularity
+						</option>
+						<option value='date'>Date</option>
+						<option value='ratings'>Ratings</option>
+						<option value='bestseller'>Best Seller</option>
+						<option value='new'>New</option>
+					</select>
+				</div>
 			</section>
 		</div>
 	);
