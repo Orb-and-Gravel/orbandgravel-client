da74e3490598dd4cc08312aae8e99e9cb5a0389f
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-12-17 19:35:45 +0400

    feat: implemented error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export function Error() {
 	return (
-		<section>
+		<section className='text-center'>
 			<h3
-				className='text-[13rem] font-black bg-error-image bg-cover bg-center'
+				className='md:text-[13rem] text-[7rem] font-black font-oswald bg-error-image bg-cover bg-center'
 				style={{
 					WebkitBackgroundClip: 'text',
 					WebkitTextFillColor: 'transparent',
@@ -12,8 +12,15 @@ export function Error() {
 			>
 				Oops!
 			</h3>
-			<h5>404 - PAGE NOT FOUND</h5>
-			<p>The page you are looking for is not available</p>
+			<h5 className='font-black tracking-wide text-xl'>404 - PAGE NOT FOUND</h5>
+			<p className='mt-5 text-sm'>
+				The page you are looking for is not available
+			</p>
+			<a href='/'>
+				<button className='text-colorOne bg-colorFive px-7 py-2 rounded-full mt-1'>
+					GO TO HOMEPAGE
+				</button>
+			</a>
 		</section>
 	);
 }
