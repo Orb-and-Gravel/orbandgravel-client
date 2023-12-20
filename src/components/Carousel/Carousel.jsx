7f35c833e1eaadb8ca4c880eb89742b803da258d
ZakriaJanjua <zakria.janjua@hotmail.com>
2023-12-20 23:12:45 +0400

    feat: products images

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -8,39 +8,39 @@ export function Carousel() {
 			<CarouselItem
 				imgLink='https://images.unsplash.com/photo-1695345272166-4efd76dd7a21?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 			<CarouselItem
 				imgLink='https://images.unsplash.com/photo-1564512480295-86e479d9b87c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 			<CarouselItem
 				imgLink='https://images.unsplash.com/photo-1638609269267-f0128098a809?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2018&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 			<CarouselItem
 				imgLink='https://images.unsplash.com/photo-1695222508573-5fd70be4c7c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1923&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 			<CarouselItem
 				imgLink='https://images.unsplash.com/photo-1597740762652-6a8a0e03cee7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1949&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 			<CarouselItem
 				imgLink='https://images.unsplash.com/photo-1613338442882-b4d4643877c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1926&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 			<CarouselItem
 				imgLink='https://plus.unsplash.com/premium_photo-1669686968068-ef4133a3e782?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1925&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 			<CarouselItem
 				imgLink='https://images.unsplash.com/photo-1695407773557-ea2dc7b3ed4a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 			<CarouselItem
 				imgLink='https://plus.unsplash.com/premium_photo-1681319269837-98a23dfb24aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80'
 				alt='alt'
-			></CarouselItem>
+			/>
 		</CarouselPics>
 	);
 }
--- src/components/Products/ProductPageImageSlider.jsx
+++ src/components/Products/ProductPageImageSlider.jsx
@@ -1,36 +1,12 @@
 import React from 'react';
 import { CarouselProductPage } from '../Carousel/CarouselProductPage';
-// import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
-
-// function NextArrow(props) {
-// 	const { className, style, onClick } = props;
-// 	return (
-// 		<button className={className} onClick={onClick}>
-// 			<ArrowDownRightIcon className='w-6' />
-// 		</button>
-// 	);
-// }
+import { CarouselSingleProductPage } from '../Carousel/CarouselSingleProductPage';
 
 export function ProductPageImageSlider({ slides }) {
-	// const settings = {
-	// 	dots: false,
-	// 	infinite: true,
-	// 	speed: 500,
-	// 	slidesToShow: 1,
-	// 	slidesToScroll: 1,
-	// 	nextArrow: <NextArrow />,
-	// 	prevArrow: <NextArrow />,
-	// };
 	return (
-		<section className=''>
+		<section className='px-10'>
+			<CarouselSingleProductPage />
 			<CarouselProductPage />
-			{/* <Slider {...settings}>
-				{slides.map((slide) => (
-					<img src={slide} alt='' />
-				))}
-			</Slider> */}
 		</section>
 	);
 }
