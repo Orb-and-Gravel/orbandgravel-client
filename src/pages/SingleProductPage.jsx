be24d1a8357353dc41264edbb86409c4f871a030
ZakriaJanjua <zakria.janjua@hotmail.com>
2024-01-30 23:28:53 +0400

    feat: dynamic color updates

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProductPageImageSlider } from '../components/Products/ProductPageImageSlider';
 import images from '../assets/images.json';
 import { StarIcon } from '@heroicons/react/24/solid';
 
 export function SingleProductPage() {
+	const [color, setColor] = useState('slate');
+
 	return (
 		<div className='md:grid block grid-cols-7'>
 			<section className='col-span-4'>
@@ -30,11 +32,32 @@ export function SingleProductPage() {
 				</div>
 				<hr className='mt-7 border border-colorFour' />
 				<div className='mt-8'>
-					<p>color green</p>
+					<p>color {color}</p>
 					<div className='flex gap-x-4 mt-3'>
-						<button className='h-8 w-8 bg-slate-500 rounded-full outline outline-1 outline-offset-2 outline-colorFour' />
-						<button className='h-8 w-8 bg-blue-500 rounded-full outline outline-1 outline-offset-2 outline-colorFour' />
-						<button className='h-8 w-8 bg-green-500 rounded-full outline outline-1 outline-offset-2 outline-colorFour' />
+						<button
+							className={`h-8 w-8 bg-slate-500 rounded-full outline ${
+								color == 'slate' ? 'outline-3' : 'outline-1'
+							} outline-offset-2 outline-colorFour`}
+							onClick={() => {
+								setColor('slate');
+							}}
+						/>
+						<button
+							className={`h-8 w-8 bg-blue-500 rounded-full outline ${
+								color == 'blue' ? 'outline-3' : 'outline-1'
+							} outline-offset-2 outline-colorFour`}
+							onClick={() => {
+								setColor('blue');
+							}}
+						/>
+						<button
+							className={`h-8 w-8 bg-green-500 rounded-full outline ${
+								color == 'green' ? 'outline-3' : 'outline-1'
+							} outline-offset-2 outline-colorFour`}
+							onClick={() => {
+								setColor('green');
+							}}
+						/>
 					</div>
 				</div>
 			</section>
