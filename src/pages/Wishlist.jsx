d2eeacd28c2da65e564f6e7dd69334d393acd95d
ZakriaJanjua <zakria.janjua@hotmail.com>
2024-02-16 00:38:30 +0400

    feat: wishlist header

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,5 +1,13 @@
+import { HeartIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 
 export function Wishlist() {
-	return <div>Wishlist</div>;
+	return (
+		<React.Fragment>
+			<section className='text-colorThree'>
+				<HeartIcon className='w-32 stroke-2' />
+				<h2 className='text-7xl font-oswald font-black'>My Wishlist</h2>
+			</section>
+		</React.Fragment>
+	);
 }
