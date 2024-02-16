7a257de79a5b965e733e5035b335f53cd4883b4e
ZakriaJanjua <zakria.janjua@hotmail.com>
2024-02-17 02:12:29 +0400

    dev: wishlist table

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,13 +1,29 @@
-import { HeartIcon } from '@heroicons/react/24/outline';
 import React from 'react';
+import { HeartIcon } from '@heroicons/react/24/solid';
 
 export function Wishlist() {
 	return (
 		<React.Fragment>
-			<section className='text-colorThree'>
-				<HeartIcon className='w-32 stroke-2' />
-				<h2 className='text-7xl font-oswald font-black'>My Wishlist</h2>
+			<section className='text-colorFive flex flex-col items-center mt-8'>
+				<h2 className='text-8xl font-oswald font-black tracking-tight'>
+					My Wishlist
+				</h2>
+				<HeartIcon className='w-28 text-rose-300' />
 			</section>
+			<table>
+				<thead>
+					<tr>
+						<th></th>
+						<th>Product Name</th>
+						<th>Price</th>
+						<th>Stock Status</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr></tr>
+				</tbody>
+			</table>
 		</React.Fragment>
 	);
 }
