d8c3c9901149999ba86e684f894a890fa3bda9f9
ZakriaJanjua <zakria.janjua@hotmail.com>
2024-02-18 02:14:24 +0400

    feat: implemented wishlist page for large screen

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { HeartIcon } from '@heroicons/react/24/solid';
+import { HeartIcon, TrashIcon } from '@heroicons/react/24/solid';
+import wishlist from '../assets/wishlist.json';
 
 export function Wishlist() {
 	return (
@@ -10,20 +11,59 @@ export function Wishlist() {
 				</h2>
 				<HeartIcon className='w-28 text-rose-300' />
 			</section>
-			<table>
-				<thead>
-					<tr>
-						<th></th>
-						<th>Product Name</th>
-						<th>Price</th>
-						<th>Stock Status</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr></tr>
-				</tbody>
-			</table>
+			<section className='px-32 mt-7'>
+				<table className='w-full'>
+					<thead className='w-full text-left'>
+						<th className='w-10 p-3'></th>
+						<th className='w-24 p-3'></th>
+						<th className='p-3'>Product Name</th>
+						<th className='p-3'>Price</th>
+						<th className='p-3'>Stock Status</th>
+						<th className='p-3'></th>
+					</thead>
+					<tbody className='w-full'>
+						{wishlist.map((item) => (
+							<tr className='w-full border-t-2' key={item.id}>
+								<td className='p-3'>
+									<TrashIcon className='w-6 cursor-pointer text-red-500 transition-all hover:text-red-600' />
+								</td>
+								<td className='p-3'>
+									<img
+										src={item.productImage}
+										alt='img'
+										className='w-16 h-20 rounded-md object-cover'
+									/>
+								</td>
+								<td className='p-3'>
+									<p>{item.productName}</p>
+								</td>
+								<td className='p-3'>{item.price}</td>
+								<td className='p-3'>
+									<p
+										className={
+											item.stockStatus ? 'text-green-700' : 'text-red-700'
+										}
+									>
+										{item.stockStatus ? 'In Stock' : 'Out of Stock'}
+									</p>
+								</td>
+								<td className='p-3'>
+									<button
+										className={`text-xs bg-colorFour text-colorOne px-5 py-2 rounded-full transition-all ${
+											item.stockStatus
+												? 'hover:bg-colorFive'
+												: 'opacity-70 cursor-not-allowed'
+										}`}
+										disabled={!item.stockStatus}
+									>
+										ADD TO CART
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</section>
 		</React.Fragment>
 	);
 }
