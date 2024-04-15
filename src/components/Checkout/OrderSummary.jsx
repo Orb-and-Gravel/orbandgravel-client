3f23fd38b50a7c57724763b6311bfdce290e50f5
ZakriaJanjua <zakria.janjua@hotmail.com>
2024-04-15 23:04:15 +0200

    feat: order summary initiated

--- src/components/Checkout/OrderSummary.jsx
+++ src/components/Checkout/OrderSummary.jsx
@@ -4,13 +4,16 @@ import cartItems from '../../assets/cartItems.json';
 export function OrderSummary() {
 	return (
 		<div className='px-8'>
-			<h3 className='font-bold mb-7 text-xl'>Order Summary</h3>
+			<h3 className='font-bold mb-5 text-xl'>Order Summary</h3>
 			<section>
 				<img
 					src={cartItems[0].imgUrl}
 					alt='img'
 					className='w-full h-48 object-cover'
 				/>
+				<div>
+					<p>{cartItems[0].name}</p>
+				</div>
 			</section>
 		</div>
 	);
--- src/components/Checkout/UserDetails.jsx
+++ src/components/Checkout/UserDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function UserDetails() {
 	return (
 		<div className='text-colorFive'>
-			<h3 className='font-bold mb-7 text-xl'>User Details</h3>
+			<h3 className='font-bold mb-5 text-xl'>User Details</h3>
 			<p className='mb-1 text-sm'>Full Name</p>
 			<input
 				type='text'
