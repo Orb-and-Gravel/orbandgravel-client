d2adb3893d3f859ddfdeab5dbeac2ffab4de8759
ZakriaJanjua <zakria.janjua@hotmail.com>
2024-05-13 02:36:26 +0200

    feat: single order template

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -12,6 +12,35 @@ export function Order() {
 					12th December 2023
 				</span>
 			</span>
+			<section>
+				<div className='mt-3'>
+					<hr className='border-1 border-colorTwo' />
+					<div className='p-4'>
+						<span className='text-sm text-gray-500'>
+							Product Code: 12122143252
+						</span>
+						<div className='mt-1 grid grid-cols-4'>
+							<img
+								src='https://media.istockphoto.com/id/1434115118/de/foto/diamant-auf-goldenem-grund.jpg?s=1024x1024&w=is&k=20&c=awKxNLURdqz1haQdL5rpTcOdZyt04T7Epbon3u89t5M='
+								alt=''
+								className='w-56 h-36 object-cover rounded-lg'
+							/>
+							<span className='flex justify-center gap-x-2 mt-1'>
+								<span className='text-sm'>color: </span>
+								<span className=' w-5 h-5 rounded-full bg-slate-400' />
+							</span>
+							<span className='text-center mt-1'>
+								<span className='text-sm'>qty: </span>
+								<span className='font-extrabold'>x2</span>
+							</span>
+							<span className='text-center text-xl font-extrabold text-colorFour mt-1'>
+								$656
+							</span>
+						</div>
+					</div>
+				</div>
+			</section>
+			<section></section>
 		</div>
 	);
 }
