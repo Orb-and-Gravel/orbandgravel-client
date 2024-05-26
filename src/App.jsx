7ed0da55c824b9689afe91e7358b069b327ba04a
ZakriaJanjua <zakria.janjua@hotmail.com>
2024-05-26 09:43:00 +0200

    feat: axios and react-query

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { OrderHistory } from './pages/OrderHistory';
 import { Order } from './pages/Order';
 import { SignIn } from './pages/SignIn';
 import { SignUp } from './pages/SignUp';
+import { axiosbase } from './axiosConfig';
 
 function PageLayout() {
 	return (
